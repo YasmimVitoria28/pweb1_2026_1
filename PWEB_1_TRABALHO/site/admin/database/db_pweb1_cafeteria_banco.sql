61b5aa86554af3b2ed731444968996f2d83e2b5e
YasmimVitoria28 <yasmim.vsr07@aluno.ifsc.edu.br>
2026-06-15 22:58:57 -0300

    15/06

--- PWEB_1_TRABALHO/site/admin/database/db_pweb1_cafeteria_banco.sql
+++ PWEB_1_TRABALHO/site/admin/database/db_pweb1_cafeteria_banco.sql
@@ -29,9 +29,12 @@ CREATE TABLE IF NOT EXISTS `avaliacao` (
   PRIMARY KEY (`id`),
   KEY `fk_avaliacao_produto` (`produto_id`),
   CONSTRAINT `fk_avaliacao_produto` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela cafeteria.avaliacao: ~2 rows (aproximadamente)
+INSERT INTO `avaliacao` (`id`, `pedido_id`, `produto_id`, `nota`, `comentario`) VALUES
+	(13, 3, 3, 4, 'Gostei'),
+	(14, 3, 3, 5, 'estava bom');
 
 -- Copiando estrutura para tabela cafeteria.pedidos
 CREATE TABLE IF NOT EXISTS `pedidos` (
@@ -40,9 +43,12 @@ CREATE TABLE IF NOT EXISTS `pedidos` (
   `nome_cafe` varchar(50) COLLATE utf8mb4_bin NOT NULL DEFAULT '',
   `valor_total` decimal(10,2) NOT NULL DEFAULT '0.00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela cafeteria.pedidos: ~2 rows (aproximadamente)
+INSERT INTO `pedidos` (`id`, `numero_pedido`, `nome_cafe`, `valor_total`) VALUES
+	(3, 10, 'caramelo', 15.00),
+	(7, 20, 'expresso', 9.00);
 
 -- Copiando estrutura para tabela cafeteria.produto
 CREATE TABLE IF NOT EXISTS `produto` (
@@ -51,9 +57,12 @@ CREATE TABLE IF NOT EXISTS `produto` (
   `preco_unitario` decimal(20,6) DEFAULT NULL,
   `categoria` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela cafeteria.produto: ~2 rows (aproximadamente)
+INSERT INTO `produto` (`id`, `nome`, `preco_unitario`, `categoria`) VALUES
+	(2, 'expresso', 5.000000, 'Cafés'),
+	(3, 'cafe gelado', 20.000000, 'Cafés');
 
 -- Copiando estrutura para tabela cafeteria.usuario
 CREATE TABLE IF NOT EXISTS `usuario` (
@@ -65,9 +74,13 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `nivel_acesso` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0',
   `login` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela cafeteria.usuario: ~3 rows (aproximadamente)
+INSERT INTO `usuario` (`id`, `nome`, `email`, `telefone`, `senha`, `nivel_acesso`, `login`) VALUES
+	(11, 'Administrador Geral', 'admin@email.com', '49999999999', '$2y$10$8Y2LyE0CpHO4YWIejv/msObRPm/XcUyBO0e/DOd1VpDHJls/QWAQy', 'admin', 'admin'),
+	(20, 'emily123', 'emily@email.com', '4999999999', '$2y$10$aKYOOdEGr3Q39xF51gHwJO7PaSRdzTL5YEHxghsaGFx1s0DPPXIEW', '0', 'emily'),
+	(21, 'emily', 'emily@email', '4999999999', '$2y$10$D7OX97BEY.w0k3G3fwlpEe.CLl/uJekQoYdMyo7HxK.OovpsfNnBu', 'cliente', 'emilia');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
